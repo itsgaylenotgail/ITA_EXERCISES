2b683611c6f628027fcbc5ad9f5d56625d3378b3
BITUIGA <BITUIGA@ZHA-ITA140-W7.corp.oocl.com>
2017-08-02 13:25:01 +0800

    chos

--- Day3_Exercises/src/exer6/Tank.java
+++ Day3_Exercises/src/exer6/Tank.java
@@ -1,12 +1,14 @@
 package exer6;
 
+import java.util.Scanner;
+
 public class Tank {
 	static boolean isEmpty;
 	static boolean isCleaned;
 	
-	public Tank() {
+	public Tank(boolean initState) {
 		System.out.println("Tank created.");
-		setEmptyState(true);		
+		setEmptyState(initState);		
 	}
 	
 	public void setEmptyState(boolean isEmp)
@@ -47,31 +49,45 @@ public class Tank {
 		System.out.println("Tank is Cleaned");
 	}
 	
-	public static void finalize(Tank tangc1) {
+	public static boolean finalize(Tank tangc1) {
 		if(tangc1.getCleanState()) {
 			System.out.println("Terminated. Tank is confirmed clean");
+			return false;
 		}
 		else {
 			System.out.println("Tank is not yet clean. Try again.");
+			return true;
 		}	
+	}
+	
+	public static boolean getInitialState()
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Empty Tank? [Y/N] ");
+		
+		String a = reader.next(); // Scans the next token of the input as an int.
+		if(a.toUpperCase() == "Y") return true;
+		else if(a.toUpperCase() == "Y") return false;
+		else return false;
 	}
 	
 	public static void main(String args[]) {
 		
-		Tank tangc1 = new Tank();
 		
-		if(tangc1.getEmptyState())tangc1.fillTank();
-		else {
-			System.out.println("Tank already full.");
-		}
-		
-		if(tangc1.getEmptyState()) tangc1.cleanTank();
-		else {
-			tangc1.emptyTank();
-			tangc1.cleanTank();
-		}
-		
-		finalize(tangc1);
+		Tank tangc1;
+		do {
+			tangc1 = new Tank(getInitialState());
+			if(tangc1.getEmptyState())tangc1.fillTank();
+			else {
+				System.out.println("Tank already full.");
+			}
+			
+			if(tangc1.getEmptyState()) tangc1.cleanTank();
+			else {
+				tangc1.emptyTank();
+				tangc1.cleanTank();
+			}
+		}while(finalize(tangc1));
 		
 	}
 	
